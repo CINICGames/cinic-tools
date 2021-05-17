01d06fbd07111fba941aa0b348b5fb0dce432d48
Matteo Sali <salimatteo@gmail.com>
2021-05-17 09:20:09 +0200

    Added prefix text to version label

--- Assets/Cinic Tools/Version Label/Scripts/VersionLabel.cs
+++ Assets/Cinic Tools/Version Label/Scripts/VersionLabel.cs
@@ -4,9 +4,10 @@ using UnityEngine;
 namespace CinicGames.Tools.Version {
     public class VersionLabel : MonoBehaviour {
         [SerializeField] private TMP_Text label = default;
+        [SerializeField] private string prefixText = "Version";
 
         private void Awake() {
-            label.text = $"Version {Application.version}";
+            label.text = $"{prefixText} {Application.version}";
         }
     }
 }
